152238f51dbf3473d4004fd5f97b3ce8e34f5e3c
khj <kasperhelweg@gmail.com>
2015-02-16 14:11:24 +0100

    fix autoloading

--- nestedpages.php
+++ nestedpages.php
@@ -48,7 +48,7 @@ function nestedpages_check_versions( $wp = '3.9', $php = '5.3.2' ) {
 
 if( !class_exists('Bootstrap') ) :
     nestedpages_check_versions();
-    require_once('vendor/autoload.php');
+    //require_once('vendor/autoload.php');
     require_once('app/NestedPages.php');
     NestedPages::init();
 endif;
